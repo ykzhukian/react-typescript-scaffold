53d6fa0162fe1e47acb9bf8d7a77f0dbb2a3c2a2
ykzhukian <kianzyk@gmail.com>
2020-07-08 22:16:59 +0800

    fix quotes

--- typing/index.d.ts
+++ typing/index.d.ts
@@ -1,9 +1,9 @@
-declare module "*.png" {
+declare module '*.png' {
   const value: any;
   export default value;
 }
 
-declare module "*.svg" {
+declare module '*.svg' {
   const value: any;
   export default value;
 }
